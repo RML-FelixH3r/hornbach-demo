5825e5ac19743ef02272f1f0ba266bd5c5bf8f39
FelixH3r <felix.hermanutz@gmx.at>
2024-01-11 08:36:55 +0100

    remove hornbach logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -31,9 +31,9 @@
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto"
-                 src="https://www.hornbach.at/header/img/hornbach_logo_de.a88b42bc1220d54085d512fa0cb330c5.svg"
-                 alt="Your Company">
+            <!--            <img class="h-8 w-auto"-->
+            <!--                 src="https://www.hornbach.at/header/img/hornbach_logo_de.a88b42bc1220d54085d512fa0cb330c5.svg"-->
+            <!--                 alt="Your Company">-->
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
