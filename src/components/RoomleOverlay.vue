44cbeb573d05a839534913694dd5e4cdb4c94053
FelixH3r <felix.hermanutz@gmx.at>
2024-03-14 07:24:38 +0100

    change id

--- src/components/RoomleOverlay.vue
+++ src/components/RoomleOverlay.vue
@@ -54,7 +54,7 @@
     console.log(configurator);
     const mainStore = useMainStore();
     mainStore.setConfigurator(configurator);
-    configurator.ui.loadObject('productionmanager:worktop');
+    configurator.ui.loadObject('productionmanager:hornbach_worktop_master');
     configurator.extended.callbacks.onUpdateParameters = (parameters) => {
       if (parameterInitializedCallback) {
         parameterInitializedCallback(parameters);
